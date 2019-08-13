28f7e5cf63edf48757e5b1970401a7ab351ac964
Jakub Krawczyk <jakub.michal.krawczyk@gmail.com>
2019-08-13 20:40:36 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Jakub Krawczyk on 13/08/2019.
+//  Copyright © 2019 Jakub Krawczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
